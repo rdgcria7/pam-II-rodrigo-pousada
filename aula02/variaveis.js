fa1ecbe7fa7086d985aa858988f189d6dcec9729
rdgcria7 <rodrigopousadaviera6@gmail.com>
2023-03-22 08:28:56 -0300

    aula sobre variaveis do javascript

---
+++ aula02/variaveis.js
@@ -0,0 +1,23 @@
+// let num1 = 2
+// const num2 = 3
+// var msg = "salve salve familia se liga nesse número:"
+
+// console.log(msg, num1)
+// console.log("da um liga nessa soma pprt kkk" ,num1, "+",num2,"=",num2 + num1)
+
+// // JSON - Javascript Object Notation
+
+// let usuario = {
+//     nome: "Rogerinho",
+//     idade: 17,
+//     signo: "Transariano kkk",
+//     zica: true
+// }
+
+// console.log (usuario.signo)
+
+const nomes = ["rodrigo","kifo"]
+
+console.log(nomes)
+console.log(typeof nomes)
+console.log(nomes[0])
